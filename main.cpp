351d6c92082e6c5447ae7f0b4d58e4b20bb56a4e
Keegan <glove.save.30@gmail.com>
2016-08-08 11:16:01 -0600

    Dielectrics

--- main.cpp
+++ main.cpp
@@ -18,6 +18,13 @@ vec3 random_in_unit_sphere()
 	return p;
 }
 
+float schlick(float cosine, float ref_idx) 
+{
+	float r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 = r0*r0;
+	return r0 + (1 - r0)*pow((1 - cosine), 5);
+}
+
 bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted)
 {
 	vec3 uv = unit_vector(v);
@@ -54,6 +61,7 @@ vec3 reflect(const vec3& v, const vec3& n) {
 
 class metal : public material {
 public:
+	metal(const vec3& a) : albedo(a) { fuzz = 0.25; } // default fuzziness
 	metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
 	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
 		vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
@@ -63,6 +71,49 @@ public:
 	}
 	vec3 albedo;
 	float fuzz;
+};
+
+class dielectric : public material {
+public:
+	dielectric(float ri) : ref_idx(ri) {}
+	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+		vec3 outward_normal;
+		vec3 reflected = reflect(r_in.direction(), rec.normal);
+		float ni_over_nt;
+		attenuation = vec3(1.0, 1.0, 1.0);
+		vec3 refracted;
+		float reflect_prob;
+		float cosine;
+		std::uniform_real_distribution<float> drand(-1.0, 1.0);
+		if (dot(r_in.direction(), rec.normal) > 0) {
+			outward_normal = -rec.normal;
+			ni_over_nt = ref_idx;
+			cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+		}
+		else {
+			outward_normal = rec.normal;
+			ni_over_nt = 1.0 / ref_idx;
+			cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+		}
+		if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+			reflect_prob = schlick(cosine, ref_idx);
+		}
+		else {
+			// (note: the first appearance of “scattered =” above is not needed and in fast is always 
+			// over-written later. Doesn’t affect result but is a performance bug).
+			// scattered = ray(rec.p, reflected);
+			reflect_prob = 1.0;
+		}
+		if (drand(generator) < reflect_prob) {
+			scattered = ray(rec.p, reflected);
+		}
+		else {
+			scattered = ray(rec.p, refracted);
+		}
+		return true;
+	}
+
+	float ref_idx;
 };
 
 
@@ -92,13 +143,15 @@ int main()
 	std::ofstream ost{ "test.ppm" };
 	ost << "P3\n" << nx << " " << ny << "\n255\n";
 
-	hitable *list[4];
-	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
-	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 1.0));
-	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 0.3));
+	const int NUM_SPHERES = 5;
+	hitable *list[NUM_SPHERES];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5))); // Blue middle
+	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0))); // Giant green that acts as ground
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2))); // Metal right
+	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5)); // These two act as a sort of glass bubble
+	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5)); // Only work together though?
 
-	hitable *world = new hitable_list(list, 4);
+	hitable *world = new hitable_list(list, NUM_SPHERES);
 	camera cam;
 	std::uniform_real_distribution<float> distribution(0, 1);
 
