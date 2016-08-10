aec2b8b3939722e3e7ccc8bf26f8b78049a6ccdf
Keegan <glove.save.30@gmail.com>
2016-08-09 20:06:30 -0600

    Finished book 1
    Fixed bug with the RNG range, was -1.0 - 1.0 when it should have been
    0.0 - 1.0. Now dielectrics work!
    Added positional camera and depth of field / defocus blur.
    Added random scene generator

--- main.cpp
+++ main.cpp
@@ -4,118 +4,11 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "camera.h"
-#include <stdlib.h>
+#include "material.h"
 
 std::default_random_engine generator;
-std::uniform_real_distribution<float> distribution(-1.0, 1.0);
-
-vec3 random_in_unit_sphere()
-{
-	vec3 p;
-	do {
-		p = vec3(distribution(generator), distribution(generator), distribution(generator));
-	} while (p.squared_length() >= 1.0);
-	return p;
-}
-
-float schlick(float cosine, float ref_idx) 
-{
-	float r0 = (1 - ref_idx) / (1 + ref_idx);
-	r0 = r0*r0;
-	return r0 + (1 - r0)*pow((1 - cosine), 5);
-}
-
-bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted)
-{
-	vec3 uv = unit_vector(v);
-	float dt = dot(uv, n);
-	float discriminant = 1.0 - ni_over_nt*ni_over_nt*(1 - dt*dt);
-	if (discriminant > 0) {
-		refracted = ni_over_nt*(uv - n*dt) - n*sqrt(discriminant);
-		return true;
-	}
-	else
-		return false;
-}
-
-class material {
-public:
-	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
-};
-
-class lambertian : public material {
-public:
-	lambertian(const vec3& a) : albedo(a) {}
-	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
-		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-		scattered = ray(rec.p, target - rec.p);
-		attenuation = albedo;
-		return true;
-	}
-	vec3 albedo;
-};
-
-vec3 reflect(const vec3& v, const vec3& n) {
-	return v - 2 * dot(v, n)*n;
-}
-
-class metal : public material {
-public:
-	metal(const vec3& a) : albedo(a) { fuzz = 0.25; } // default fuzziness
-	metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
-	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
-		vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-		scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
-		attenuation = albedo;
-		return (dot(scattered.direction(), rec.normal) > 0);
-	}
-	vec3 albedo;
-	float fuzz;
-};
-
-class dielectric : public material {
-public:
-	dielectric(float ri) : ref_idx(ri) {}
-	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
-		vec3 outward_normal;
-		vec3 reflected = reflect(r_in.direction(), rec.normal);
-		float ni_over_nt;
-		attenuation = vec3(1.0, 1.0, 1.0);
-		vec3 refracted;
-		float reflect_prob;
-		float cosine;
-		std::uniform_real_distribution<float> drand(-1.0, 1.0);
-		if (dot(r_in.direction(), rec.normal) > 0) {
-			outward_normal = -rec.normal;
-			ni_over_nt = ref_idx;
-			cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
-		}
-		else {
-			outward_normal = rec.normal;
-			ni_over_nt = 1.0 / ref_idx;
-			cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
-		}
-		if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
-			reflect_prob = schlick(cosine, ref_idx);
-		}
-		else {
-			// (note: the first appearance of “scattered =” above is not needed and in fast is always 
-			// over-written later. Doesn’t affect result but is a performance bug).
-			// scattered = ray(rec.p, reflected);
-			reflect_prob = 1.0;
-		}
-		if (drand(generator) < reflect_prob) {
-			scattered = ray(rec.p, reflected);
-		}
-		else {
-			scattered = ray(rec.p, refracted);
-		}
-		return true;
-	}
-
-	float ref_idx;
-};
-
+//std::uniform_real_distribution<float> distribution(-1.0, 1.0);
+std::uniform_real_distribution<float> drand(0, 1.0);
 
 vec3 color(const ray& r, hitable *world, int depth)
 {
@@ -135,14 +28,55 @@ vec3 color(const ray& r, hitable *world, int depth)
 	}
 }
 
+hitable *random_scene() {
+	int n = 500;
+	hitable **list = new hitable*[n + 1];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			float choose_mat = drand(generator);
+			vec3 center(a + 0.9 + drand(generator), 0.2, b + 0.9*drand(generator));
+			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+				if (choose_mat < 0.8) { // diffuse
+					list[i++] = new sphere(center, 0.2, new lambertian(
+						vec3(drand(generator)*drand(generator),
+						drand(generator)*drand(generator),
+						drand(generator)*drand(generator))));
+				}
+				else if (choose_mat < 0.95) { // metal
+					list[i++] = new sphere(center, 0.2, new metal(
+						vec3(0.5*(1 + drand(generator)),
+						0.5*(1 + drand(generator)),
+						0.5*(1 + drand(generator))),
+						0.5*drand(generator)));
+				}
+				else { // glass
+					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+				}
+			}
+		}
+	}
+
+	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+	return new hitable_list(list, i);
+}
+
 int main()
 {
-	int nx = 200;
-	int ny = 100;
-	int ns = 100;
-	std::ofstream ost{ "test.ppm" };
+	// Seed RNG
+	generator.seed(std::random_device()());
+	int nx = 1200;
+	int ny = 800;
+	int ns = 10;
+	std::ofstream ost{ "scene.ppm" };
 	ost << "P3\n" << nx << " " << ny << "\n255\n";
 
+	const double M_PI = 3.14159265358979323846;
+	
 	const int NUM_SPHERES = 5;
 	hitable *list[NUM_SPHERES];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5))); // Blue middle
@@ -150,17 +84,22 @@ int main()
 	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2))); // Metal right
 	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5)); // These two act as a sort of glass bubble
 	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5)); // Only work together though?
-
+	
 	hitable *world = new hitable_list(list, NUM_SPHERES);
-	camera cam;
-	std::uniform_real_distribution<float> distribution(0, 1);
+	world = random_scene();
+	vec3 lookfrom(13, 2, 3);
+	vec3 lookat(0, 0, 0);
+	//float dist_to_focus = (lookfrom - lookat).length();
+	float dist_to_focus = 10.0;
+	float aperture = 0.1;
+	camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
 
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
 			vec3 col(0, 0, 0);
 			for (int s = 0; s < ns; s++) {				
-				float u = float(i + distribution(generator)) / float(nx);
-				float v = float(j + distribution(generator)) / float(ny);
+				float u = float(i + drand(generator)) / float(nx);
+				float v = float(j + drand(generator)) / float(ny);
 				ray r = cam.get_ray(u, v);
 				vec3 p = r.point_at_parameter(2.0);
 				col += color(r, world, 0);
